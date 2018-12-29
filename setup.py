4ba872b47af4e8ae78fecc6531e202dc68687255
Seth M. Larson <sethmichaellarson@gmail.com>
2018-12-29 16:57:30 -0600

    Switch to using setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup (
-    name = 'win_inet_pton',
-    version = '1.0.1',
-    py_modules = ['win_inet_pton'],
-    url = 'https://github.com/hickeroar/win_inet_pton',
-    author = 'Ryan Vennell',
-    author_email = 'ryan.vennell@gmail.com',
-    description = 'Native inet_pton and inet_ntop implementation for Python on Windows (with ctypes).',
-    license = open('LICENSE', 'r').read(),
-    classifiers = [
+setup(
+    name='win_inet_pton',
+    version='1.0.1',
+    py_modules=['win_inet_pton'],
+    url='https://github.com/hickeroar/win_inet_pton',
+    author='Ryan Vennell',
+    author_email='ryan.vennell@gmail.com',
+    description='Native inet_pton and inet_ntop implementation for Python on Windows (with ctypes).',
+    license=open('LICENSE', 'r').read(),
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
